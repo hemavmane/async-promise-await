d010d5598072a9fffca12ea1344aee1ace3f3d5f
Admin <hemamane5376@gmail.com>
2022-08-19 19:02:20 +0530

    promise

---
+++ file.js
@@ -0,0 +1,65 @@
+// <!-- callback : callback is a function which passed as parametr -->
+// <!-- asynchronous programme -->
+
+// function students(){
+//     return new Promise((resolve,reject)=>{
+//         let student =false
+//         if(student){
+            
+//             resolve("he is student")
+//         }else{
+//             reject("he is not a student")
+            
+//         }
+//     })
+// }
+
+
+// async function getmark(){
+// try{
+//     let msg= await students()
+//     console.log(msg)
+    
+// }catch(err){
+//     console.log(err);
+// }
+
+// }
+// getmark()
+
+
+// async await
+
+// function getdetails(){
+//     return new Promise((resolve,reject)=>{
+//         let adharcard=""
+//         let person=false
+//         if(person){
+//             resolve("he is indian")
+//         }else{
+//             reject("anti india")
+//         }
+//     })
+// }
+
+
+// async function details(){
+//    try{
+//     let documents = await getdetails()
+//     console.log(documents);
+//    }catch(err){
+//     console.log(err);
+//    } 
+// }details()
+
+
+
+
+
+
+
+
+
+
+
+
